014702161c938d44bdb2239c9e6a885759102eb9
Alan Kuźnicki <A.L.Kuznicki@student.tudelft.nl>
2022-12-08 13:48:16 +0100

    Added Javadoc to the NodeRepository class

--- cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/domain/cluster/NodeRepository.java
+++ cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/domain/cluster/NodeRepository.java
@@ -11,12 +11,40 @@ import java.util.Optional;
 @Repository
 public interface NodeRepository extends JpaRepository<Node, Long> {
 
+    /**
+     * Tries to find and return a node by node ID.
+     *
+     * @param id the id to find the node by
+     *
+     * @return an optional containing the node if found
+     */
     Optional<Node> findById(long id);
 
+    /**
+     * Checks if a node exists with the given ID.
+     *
+     * @param id the id to check the existence of
+     *
+     * @return a boolean indicating whether a node with the given ID exists in the repository
+     */
     boolean existsById(long id);
 
+    /**
+     * Tries to find and return a node by node url.
+     *
+     * @param url the url to find the node by
+     *
+     * @return an optional containing the node if found
+     */
     Optional<Node> findByUrl(String url);
 
+    /**
+     * Checks if a node exists with the given url.
+     *
+     * @param url the url to check the existence of
+     *
+     * @return a boolean indicating whether a node with the given url exists in the repository
+     */
     boolean existsByUrl(String url);
 
 }
