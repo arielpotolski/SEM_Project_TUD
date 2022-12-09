64131116947c966d4e69c6fe213f752e597d572d
Alan Kuźnicki <A.L.Kuznicki@student.tudelft.nl>
2022-12-10 00:57:13 +0100

    Added service class for assigning nodes to faculties.

---
+++ cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/domain/cluster/NodeAssignmentService.java
@@ -0,0 +1,10 @@
+package nl.tudelft.sem.template.cluster.domain.cluster;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * This class provides a service which assigns a given node to a faculty based on the chosen strategy.
+ */
+@Service
+public class NodeAssignmentService {
+}
