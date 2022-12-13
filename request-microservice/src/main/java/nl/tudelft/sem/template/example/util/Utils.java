2fdb7da1867ffad0dbd124fd48dabfe4a36e7d16
Vladimir Pavlov <v.v.pavlov-1@student.tudelft.nl>
2022-12-13 16:19:29 +0100

    fixed small bug

--- request-microservice/src/main/java/nl/tudelft/sem/template/example/util/Utils.java
+++ request-microservice/src/main/java/nl/tudelft/sem/template/example/util/Utils.java
@@ -1,11 +1,21 @@
 package nl.tudelft.sem.template.example.util;
 
+/**
+ * The type Utils.
+ */
 public class Utils {
 
 
-    public static boolean idIsContained(Long[] ids,long id){
-        for (Long aLong : ids) {
-            if (aLong == id) {
+    /**
+     * Checks if a given id is contained within a given array.
+     *
+     * @param ids the ids
+     * @param id  the id
+     * @return the boolean
+     */
+    public static boolean idIsContained(Long[] ids, long id) {
+        for (Long along : ids) {
+            if (along == id) {
                 return true;
             }
         }
