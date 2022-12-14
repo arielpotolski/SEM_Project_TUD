81670337c1d02d05cce1f3ddd3859b57049e354d
Bart Coster <b.i.coster@student.tudelft.nl>
2022-12-14 02:20:54 +0100

    CheckStyle

--- cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/authentication/JwtRequestFilter.java
+++ cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/authentication/JwtRequestFilter.java
@@ -2,6 +2,12 @@ package nl.tudelft.sem.template.cluster.authentication;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import nl.tudelft.sem.template.cluster.authentication.JwtTokenVerifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,13 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
 
 
 /**
