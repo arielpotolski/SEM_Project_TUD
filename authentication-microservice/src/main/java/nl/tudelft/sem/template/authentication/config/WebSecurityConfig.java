486beb2c6df36d3616c03eb5c63344f37688dc28
Kasper van Duijne <K.W.vanDuijne@student.tudelft.nl>
2022-12-18 20:57:12 +0100

    Added endpoint for getting all existing faculties, and added this endpoint to be exempt from authentication.

--- authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/config/WebSecurityConfig.java
+++ authentication-microservice/src/main/java/nl/tudelft/sem/template/authentication/config/WebSecurityConfig.java
@@ -72,7 +72,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().antMatchers("/register", "/authenticate").permitAll().and()
+                .authorizeRequests().antMatchers("/register", "/authenticate", "/getAllFaculties").permitAll().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
