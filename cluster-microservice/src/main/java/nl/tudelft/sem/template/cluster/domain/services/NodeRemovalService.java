53e78da66b896815bd5f5c768a5c3e5fd2e13a65
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-12-20 04:39:15 +0100

    Changes the nodeRemovalService to a correct implementation.

--- cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/domain/services/NodeRemovalService.java
+++ cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/domain/services/NodeRemovalService.java
@@ -2,21 +2,23 @@ package nl.tudelft.sem.template.cluster.domain.services;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import nl.tudelft.sem.template.cluster.domain.cluster.Node;
 import nl.tudelft.sem.template.cluster.domain.cluster.NodeRepository;
-import nl.tudelft.sem.template.cluster.domain.providers.implementations.TimeToMidnightProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableAsync
+@EnableScheduling
 public class NodeRemovalService {
 
     private List<Node> nodesToRemove;  // list of nodes to be removed after midnight.
-    private NodeRepository repo;
-    private TimeToMidnightProvider timeProvider;
+
+    private NodeRepository nodeRepo;
 
     /**
      * Constructor of the service class.
@@ -26,7 +28,7 @@ public class NodeRemovalService {
     @Autowired
     public NodeRemovalService(NodeRepository repo) {
         this.nodesToRemove = new ArrayList<>();
-        this.repo = repo;
+        this.nodeRepo = repo;
     }
 
     /**
@@ -53,7 +55,7 @@ public class NodeRemovalService {
      * @return the repository of this service
      */
     public NodeRepository getRepo() {
-        return this.repo;
+        return this.nodeRepo;
     }
 
     /**
@@ -61,59 +63,23 @@ public class NodeRemovalService {
      *
      * @param node node to be added to the list
      */
-    public void addNodeToBeRemoved(Node node) {
+    public boolean addNodeToBeRemoved(Node node) {
         this.nodesToRemove.add(node);
+        return true;
     }
 
     /**
      * This method removes the nodes in the nodesToRemoveList every time it's midnight.
-     * The arguments in the schedule method are:
-     * Remove: inner class that implements a runnable task (removing the nodes from the
-     * 			list and from the nodeRepo);
-     * timeProvider.timeToMidnight(): a method in a provider that calculates how much time
-     * 			there is left to midnight (in Milliseconds);
-     * TimeUnit.Milliseconds: defines the time unit to milliseconds.
      */
+    @Scheduled(cron = "0 33 4 * * *")
+    @Async
     public void removeNodesAtMidnight() {
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-        service.schedule(new Remove(this), this.timeProvider.timeToMidnight(), TimeUnit.MILLISECONDS);
-    }
-
-    /**
-     * This inner class implements the runnable argument required in the
-     * schedule method. This class implements the removal of the nodes in
-     * the list of nodesToRemove at midnight.
-     */
-    private class Remove implements Runnable {
-
-        private NodeRemovalService service;
-
-        /**
-         * Constructor of the class. Initializes a new NodeRemovalService,
-         * which will allow us to remove the nodes from the list nodesToRemove,
-         * in the NodeRemovalService class.
-         *
-         * @param service the service to initialize the field of this class.
-         */
-        public Remove(NodeRemovalService service) {
-            this.service = service;
+        System.out.println(this.nodeRepo.count());
+        for (Node node : this.nodesToRemove) {
+            Node n = this.nodeRepo.findByUrl(node.getUrl());
+            this.nodeRepo.delete(n);
         }
-
-        @Override
-        public void run() {
-            removeNodes();
-        }
-
-        /**
-         * Removes the nodes from the list nodesToRemove in the NodeRemovalService
-         * class, as well as from the nodeRepository.
-         */
-        public void removeNodes() {
-            List<Node> temp = this.service.getNodesToRemove();
-            for (Node node : temp) {
-                this.service.nodesToRemove.remove(node);
-                this.service.repo.delete(node);
-            }
-        }
+        System.out.println(this.nodeRepo.count());
+        this.nodesToRemove = new ArrayList<>();
     }
 }
