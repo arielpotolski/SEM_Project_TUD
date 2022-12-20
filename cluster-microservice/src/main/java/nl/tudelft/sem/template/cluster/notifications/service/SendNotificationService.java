748a0029e67d06f9b55bdf6ea2870ef9b5981e63
Ariel Potolski Eilat <A.PotolskiEilat@student.tudelft.nl>
2022-12-20 21:24:50 +0100

    Add a SendNotificationService class to the cluster microservice.

---
+++ cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/notifications/service/SendNotificationService.java
@@ -0,0 +1,54 @@
+package nl.tudelft.sem.template.cluster.notifications.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import nl.tudelft.sem.template.cluster.notifications.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SendNotificationService {
+
+    private final RestTemplate restTemplate;
+
+    /**
+     * Instantiates a new sendNotificationService.
+     *
+     * @param restTemplateBuilder the rest template builder
+     */
+    @Autowired
+    public SendNotificationService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    /**
+     * Send a notification to the endpoint receiveJobNotification in the NotificationController
+     * in the authentication microservice.
+     *
+     * @param notification the notification to be sent
+     * @throws JsonProcessingException when the notification could not be sent
+     */
+    public void sendNotificationToUser(Notification notification) throws JsonProcessingException {
+        try {
+            String url = "https://localhost:8081/notification";
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            String json = ow.writeValueAsString(notification);
+            ResponseEntity<String> result = restTemplate.postForEntity(url, json, String.class);
+            if (result.getBody().equals("ok")) {
+                return;
+            }
+        } catch (Exception e) {
+            System.out.println("error with post: " + e);
+        }
+    }
+
+}
