c1b440307cab10712f51b74cb88ee8182e364ced
Alan Kuźnicki <A.L.Kuznicki@student.tudelft.nl>
2022-12-22 12:03:29 +0100

    Fixed checkstyle indentation and javadoc issues

--- request-microservice/src/main/java/nl/tudelft/sem/template/example/domain/JobRequestRequestModel.java
+++ request-microservice/src/main/java/nl/tudelft/sem/template/example/domain/JobRequestRequestModel.java
@@ -9,19 +9,26 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class JobRequestRequestModel {
-     private String facultyId;
-     private String userNetId;
-     private String jobName;
-     private String jobDescription;
-     private double requiredCpu;
-     private double requiredGpu;
-     private double requiredMemory;
-     private LocalDate preferredCompletionDate;
+    private String facultyId;
+    private String userNetId;
+    private String jobName;
+    private String jobDescription;
+    private double requiredCpu;
+    private double requiredGpu;
+    private double requiredMemory;
+    private LocalDate preferredCompletionDate;
 
-     public static JobRequestRequestModel convertToRequestModel(Request request) {
-         return new JobRequestRequestModel(request.getFaculty(), request.getNetId(),
-                 request.getName(), request.getDescription(),
-                 request.getCpu(), request.getGpu(), request.getMemory(),
-                 request.getPreferredDate());
-     }
+    /**
+     * Converts this request to a request model so that it can be read by the cluster.
+     *
+     * @param request the request to be converted.
+     *
+     * @return the request model to be sent.
+     */
+    public static JobRequestRequestModel convertToRequestModel(Request request) {
+        return new JobRequestRequestModel(request.getFaculty(), request.getNetId(),
+                request.getName(), request.getDescription(),
+                request.getCpu(), request.getGpu(), request.getMemory(),
+                request.getPreferredDate());
+    }
 }
