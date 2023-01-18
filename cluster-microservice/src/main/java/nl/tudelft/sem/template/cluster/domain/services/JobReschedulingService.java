a52e44abd80bee8dcf4f1512d7bad4670616d5f0
Bart Coster <b.i.coster@student.tudelft.nl>
2023-01-18 16:34:12 +0100

    Refracturing JobScheduleingService
    Splitting JobScheduleingService in 2 seperate classes

---
+++ cluster-microservice/src/main/java/nl/tudelft/sem/template/cluster/domain/services/JobReschedulingService.java
@@ -0,0 +1,158 @@
+package nl.tudelft.sem.template.cluster.domain.services;
+
+import lombok.Getter;
+import nl.tudelft.sem.template.cluster.domain.cluster.AvailableResourcesForDate;
+import nl.tudelft.sem.template.cluster.domain.cluster.Job;
+import nl.tudelft.sem.template.cluster.domain.events.NotificationEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+@Getter
+@Service
+public class JobReschedulingService {
+
+    /**
+     * Provides access to information related to the resources.
+     */
+    private final transient SchedulingDataProcessingService schedulingDataProcessingService;
+
+    private final transient ApplicationEventPublisher publisher;
+
+    private transient JobSchedulingService jobSchedulingService;
+
+    @Autowired
+    public JobReschedulingService(SchedulingDataProcessingService schedulingDataProcessingService, ApplicationEventPublisher applicationEventPublisher, JobSchedulingService jobSchedulingService){
+        this.jobSchedulingService = jobSchedulingService;
+        this.schedulingDataProcessingService = schedulingDataProcessingService;
+        this.publisher = applicationEventPublisher;
+    }
+
+
+
+
+    /**
+     * This is triggered by the NodesWereRemovedEvent. The listener extracts all facultyIds from the removed nodes and
+     * sends the unique values to this method. This method then goes over each of the faculties, checking whether the
+     * available resources for any day have gone below zero, which indicates that rescheduling is necessary. Then,
+     * for each such day, jobs are removed and put in a temporary list. Once available resources are non-negative again,
+     * the method moves on to other days and then faculties. At the end of these loops, there should no longer be days
+     * when more resources are reserved than available.
+     * After that, the method iterates over temp, and for each job
+     * checks whether it can ever be scheduled (i.e., if its resource requirement does not exceed its faculty's new
+     * total assigned resources after removal of some nodes.) If it cannot, it is dropped and a notification sent to the
+     * user who requested the job. If it can be, scheduleJob is called to insert the job back into the schedule. A delay
+     * notification is sent to the user along with the new scheduled date.
+     *
+     * @param faculties all the faculties who have lost nodes in the removal that triggered the event.
+     */
+    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    public void rescheduleJobsForFacultiesWithRemovedNodes(List<String> faculties) {
+        // temp
+        List<Job> jobsToReschedule = new ArrayList<>();
+
+        // accessing repository only once to optimize
+        List<Job> fullSchedule = this.schedulingDataProcessingService.getAllJobsFromSchedule();
+
+        // available resources for each faculty
+        for (String facultyId : faculties) {
+            // this list will only contain dates with the resources, when at least one resource's balance is negative.
+            // this means that some jobs need to be rescheduled
+            var resourcesForDaysWhereReschedulingNecessary =
+                    this.schedulingDataProcessingService
+                            .getAvailableResourcesForGivenFacultyUntilDay(facultyId,
+                                    this.schedulingDataProcessingService.findLatestDateWithReservedResources()).stream()
+                            .filter(x -> x.getAvailableCpu() < 0
+                                    || x.getAvailableGpu() < 0
+                                    || x.getAvailableMemory() < 0)
+                            .collect(Collectors.toList());
+
+            // this faculty needs no rescheduling - all the jobs are still within available resources
+            if (!resourcesForDaysWhereReschedulingNecessary.isEmpty()) {
+                // go over all days with insufficient resources
+                jobsToReschedule.addAll(removeScheduledJobs(
+                        fullSchedule, resourcesForDaysWhereReschedulingNecessary, facultyId));
+            }
+        }
+        rescheduleJobs(jobsToReschedule);
+
+
+
+    }
+
+    /**
+     * This is a helper function for rescheduling jobs where nodes were removed,
+     * it removes jobs until there are enough resources.
+     * And gives back a list of the removed jobs.
+     *
+     * @param fullSchedule a schedule of all the jobs that are already scheduled
+     * @param resourcesForDaysWhereReschedulingNecessary A list of the resources for the certain days
+     * @param facultyId the faculty at hand
+     * @return List of jobs that need to be rescheduled
+     */
+    public List<Job> removeScheduledJobs(List<Job> fullSchedule, List<AvailableResourcesForDate>
+            resourcesForDaysWhereReschedulingNecessary, String facultyId) {
+        List<Job> jobsToReschedule = new ArrayList<>();
+        for (AvailableResourcesForDate lackingResources : resourcesForDaysWhereReschedulingNecessary) {
+            // get jobs scheduled for that date and faculty sorted descendingly by total cost
+            var jobsOnProblematicDay = fullSchedule.stream()
+                    .filter(x -> x.getFacultyId().equals(facultyId))
+                    .filter(x -> x.getScheduledFor().equals(lackingResources.getDate()))
+                    .sorted(Comparator.comparingDouble(
+                            (Job x) -> x.getRequiredCpu() + x.getRequiredGpu() + x.getRequiredMemory()
+                    ).reversed())
+                    .collect(Collectors.toList());
+
+            // remove until all available values are non-negative
+            double cpu = lackingResources.getAvailableCpu();
+            double gpu = lackingResources.getAvailableGpu();
+            double memory = lackingResources.getAvailableMemory();
+            while (!jobsOnProblematicDay.isEmpty() && (cpu < 0 || gpu < 0 || memory < 0)) {
+                var removedJob = jobsOnProblematicDay.remove(0); // the most costly job
+                this.schedulingDataProcessingService.deleteJob(removedJob);
+
+                // append to temp to reschedule
+                jobsToReschedule.add(removedJob);
+
+                // update resources
+                cpu += removedJob.getRequiredCpu();
+                gpu += removedJob.getRequiredGpu();
+                memory += removedJob.getRequiredMemory();
+            }
+        }
+        return jobsToReschedule;
+    }
+
+    /** This method is a helper function for rescheduling jobs if nodes are removed.
+     * It also sends a notification on the job status.
+     *
+     * @param jobsToReschedule A list of jobs to schedule agian.
+     */
+    public void rescheduleJobs(List<Job> jobsToReschedule) {
+
+        for (Job jobToReschedule : jobsToReschedule) {
+            // check if job can ever be scheduled, drop if no
+            if (!jobSchedulingService.checkIfJobCanBeScheduled(jobToReschedule)) {
+                //send notification of dropping
+                publisher.publishEvent(
+                        new NotificationEvent(this, jobToReschedule.getScheduledFor().toString(), "JOB",
+                                "DROPPED", "Your job has been dropped by the cluster!"
+                                + " We are sorry for the inconvenience.", jobToReschedule.getUserNetId()
+                        ));
+                continue;
+            }
+
+            // reschedule if yes
+            jobSchedulingService.scheduleJob(jobToReschedule);
+            publisher.publishEvent(
+                    new NotificationEvent(this, jobToReschedule.getScheduledFor().toString(), "JOB",
+                            "RESCHEDULED", "Your job has been rescheduled by the cluster!",
+                            jobToReschedule.getUserNetId()
+                    ));
+        }
+    }
+}
---
+++ cluster-microservice/src/test/java/nl/tudelft/sem/template/cluster/services/JobReschedulingServiceTest.java
@@ -0,0 +1,232 @@
+package nl.tudelft.sem.template.cluster.services;
+
+import nl.tudelft.sem.template.cluster.domain.builders.JobBuilder;
+import nl.tudelft.sem.template.cluster.domain.builders.NodeBuilder;
+import nl.tudelft.sem.template.cluster.domain.cluster.Job;
+import nl.tudelft.sem.template.cluster.domain.cluster.Node;
+import nl.tudelft.sem.template.cluster.domain.providers.DateProvider;
+import nl.tudelft.sem.template.cluster.domain.services.JobReschedulingService;
+import nl.tudelft.sem.template.cluster.domain.services.JobSchedulingService;
+import nl.tudelft.sem.template.cluster.models.FacultyDatedResourcesResponseModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class JobReschedulingServiceTest {
+
+    @Autowired
+    private transient JobSchedulingService jobSchedulingService;
+
+    @Autowired
+    private transient JobReschedulingService jobReschedulingService;
+    @Autowired
+    private transient DateProvider dateProvider;
+
+    private Node node1;
+    private Node node2;
+
+    private Job job1;
+    private Job job2;
+
+    @BeforeEach
+    void setUp() {
+        this.jobSchedulingService.getSchedulingDataProcessingService().deleteAllJobsScheduled();
+        this.jobSchedulingService.getNodeDataProcessingService().deleteAllNodes();
+
+        this.node1 = new NodeBuilder()
+                .setNodeCpuResourceCapacityTo(0.0)
+                .assignToFacultyWithId("EWI")
+                .setNodeGpuResourceCapacityTo(0.0)
+                .setNodeMemoryResourceCapacityTo(0.0)
+                .withNodeName("FacultyCentralCore")
+                .foundAtUrl("/" + "EWI" + "/central-core")
+                .byUserWithNetId("SYSTEM")
+                .constructNodeInstance();
+        this.node2 = new NodeBuilder()
+                .setNodeCpuResourceCapacityTo(10.0)
+                .setNodeGpuResourceCapacityTo(10.0)
+                .setNodeMemoryResourceCapacityTo(10.0)
+                .withNodeName("FacultyCentralCore")
+                .foundAtUrl("/" + "AE" + "/central-core")
+                .byUserWithNetId("SYSTEM")
+                .assignToFacultyWithId("AE").constructNodeInstance();
+        this.job1 = new JobBuilder().preferredCompletedBeforeDate(LocalDate.of(2023, 1, 26))
+                .needingMemoryResources(1.0).needingGpuResources(1.0).needingCpuResources(5.0).withDescription("desc")
+                .havingName("job").requestedByUserWithNetId("ariel").requestedThroughFaculty("EWI")
+                .constructJobInstance();
+        this.job1.setScheduledFor(LocalDate.of(2022, 12, 14));
+        this.job2 = new JobBuilder().preferredCompletedBeforeDate(LocalDate.of(2023, 1, 26))
+                .needingMemoryResources(1.0).needingGpuResources(1.0).needingCpuResources(2.0).withDescription("desc")
+                .havingName("ob").requestedByUserWithNetId("ariel").requestedThroughFaculty("AE")
+                .constructJobInstance();
+        this.job2.setScheduledFor(LocalDate.of(2022, 12, 14));
+    }
+
+    @Test
+    public void simpleReschedulingWithoutDroppingTest() {
+        this.jobSchedulingService.getSchedulingDataProcessingService().deleteAllJobsScheduled();
+        this.jobSchedulingService.getNodeDataProcessingService().deleteAllNodes();
+
+        this.node1.setCpuResources(5);
+        this.node1.setMemoryResources(4);
+        this.node1.setGpuResources(3);
+        this.node1.setFacultyId("AE");
+
+        this.jobSchedulingService.getNodeDataProcessingService().save(this.node1);
+
+        this.node2.setCpuResources(3);
+        this.node2.setMemoryResources(2);
+        this.node2.setGpuResources(1);
+        this.node2.setFacultyId("AE");
+
+        this.jobSchedulingService.getNodeDataProcessingService().save(this.node2);
+
+        this.job1.setRequiredGpu(4.0);
+        this.job1.setRequiredMemory(5.0);
+        this.job1.setFacultyId("AE");
+        this.job1.setScheduledFor(dateProvider.getTomorrow());
+
+        this.jobSchedulingService.getSchedulingDataProcessingService().saveInSchedule(this.job1);
+
+        this.job2.setRequiredCpu(4.0);
+        this.job2.setRequiredGpu(2.0);
+        this.job2.setRequiredMemory(2.0);
+        this.job2.setFacultyId("AE");
+        this.job2.setScheduledFor(dateProvider.getTomorrow());
+
+        this.jobSchedulingService.getSchedulingDataProcessingService().saveInSchedule(this.job2);
+
+        // we pretend that a node has been removed. There is too many reservations, and they should be rescheduled.
+        this.jobReschedulingService.rescheduleJobsForFacultiesWithRemovedNodes(List.of("AE"));
+
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .findLatestDateWithReservedResources().equals(dateProvider.getTomorrow())).isFalse();
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .getAvailableResourcesForGivenFacultyForGivenDay(dateProvider.getTomorrow(), "AE"))
+                .isEqualTo(List.of(new FacultyDatedResourcesResponseModel(dateProvider.getTomorrow(), "AE", 4.0, 2.0, 4.0)));
+    }
+
+    @Test
+    public void simpleReschedulingWithoutDroppingEnoughCpuTest() {
+        this.jobSchedulingService.getSchedulingDataProcessingService().deleteAllJobsScheduled();
+        this.jobSchedulingService.getNodeDataProcessingService().deleteAllNodes();
+
+        this.node1.setCpuResources(5);
+        this.node1.setMemoryResources(4);
+        this.node1.setGpuResources(3);
+        this.node1.setFacultyId("AE");
+
+        this.jobSchedulingService.getNodeDataProcessingService().save(this.node1);
+
+        this.node2.setCpuResources(3);
+        this.node2.setMemoryResources(2);
+        this.node2.setGpuResources(1);
+        this.node2.setFacultyId("AE");
+
+        this.jobSchedulingService.getNodeDataProcessingService().save(this.node2);
+
+        this.job1.setRequiredCpu(3.0);
+        this.job1.setRequiredGpu(4.0);
+        this.job1.setRequiredMemory(5.0);
+        this.job1.setFacultyId("AE");
+        this.job1.setScheduledFor(dateProvider.getTomorrow());
+
+        this.jobSchedulingService.getSchedulingDataProcessingService().saveInSchedule(this.job1);
+
+        this.job2.setRequiredCpu(4.0);
+        this.job2.setRequiredGpu(2.0);
+        this.job2.setRequiredMemory(2.0);
+        this.job2.setFacultyId("AE");
+        this.job2.setScheduledFor(dateProvider.getTomorrow());
+
+        this.jobSchedulingService.getSchedulingDataProcessingService().saveInSchedule(this.job2);
+
+        // we pretend that a node has been removed. There is too many reservations, and they should be rescheduled.
+        this.jobReschedulingService.rescheduleJobsForFacultiesWithRemovedNodes(List.of("AE"));
+
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .findLatestDateWithReservedResources().equals(dateProvider.getTomorrow())).isFalse();
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .getAvailableResourcesForGivenFacultyForGivenDay(dateProvider.getTomorrow(), "AE"))
+                .isEqualTo(List.of(new FacultyDatedResourcesResponseModel(dateProvider.getTomorrow(), "AE", 4.0, 2.0, 4.0)));
+    }
+
+    @Test
+    public void droppingTest() {
+        this.jobSchedulingService.getSchedulingDataProcessingService().deleteAllJobsScheduled();
+        this.jobSchedulingService.getNodeDataProcessingService().deleteAllNodes();
+
+        this.node1.setFacultyId("AE");
+        this.jobSchedulingService.getNodeDataProcessingService().save(node1);
+
+        this.job1.setRequiredCpu(3.0);
+        this.job1.setRequiredGpu(4.0);
+        this.job1.setRequiredMemory(5.0);
+        this.job1.setFacultyId("AE");
+        this.job1.setScheduledFor(dateProvider.getTomorrow().plusDays(3));
+
+        this.jobSchedulingService.getSchedulingDataProcessingService().saveInSchedule(this.job1);
+
+        this.jobReschedulingService.rescheduleJobsForFacultiesWithRemovedNodes(List.of("AE"));
+
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .findLatestDateWithReservedResources().equals(dateProvider.getTomorrow())).isTrue();
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .existsInScheduleByFacultyId("AE")).isFalse();
+    }
+
+    @Test
+    public void missingMemoryReschedulingTest() {
+        this.jobSchedulingService.getSchedulingDataProcessingService().deleteAllJobsScheduled();
+        this.jobSchedulingService.getNodeDataProcessingService().deleteAllNodes();
+
+        this.node1.setCpuResources(5);
+        this.node1.setMemoryResources(4);
+        this.node1.setGpuResources(3);
+        this.node1.setFacultyId("AE");
+
+        this.jobSchedulingService.getNodeDataProcessingService().save(this.node1);
+
+        this.node2.setCpuResources(3);
+        this.node2.setMemoryResources(2);
+        this.node2.setGpuResources(1);
+        this.node2.setFacultyId("AE");
+
+        this.jobSchedulingService.getNodeDataProcessingService().save(this.node2);
+
+        this.job1.setRequiredCpu(3.0);
+        this.job1.setRequiredGpu(1.0);
+        this.job1.setRequiredMemory(5.0);
+        this.job1.setFacultyId("AE");
+        this.job1.setScheduledFor(dateProvider.getTomorrow());
+
+        this.jobSchedulingService.getSchedulingDataProcessingService().saveInSchedule(this.job1);
+
+        this.job2.setRequiredCpu(4.0);
+        this.job2.setRequiredGpu(2.0);
+        this.job2.setRequiredMemory(2.0);
+        this.job2.setFacultyId("AE");
+        this.job2.setScheduledFor(dateProvider.getTomorrow());
+
+        this.jobSchedulingService.getSchedulingDataProcessingService().saveInSchedule(this.job2);
+
+        // we pretend that a node has been removed. There is too many reservations, and they should be rescheduled.
+        this.jobReschedulingService.rescheduleJobsForFacultiesWithRemovedNodes(List.of("AE"));
+
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .findLatestDateWithReservedResources().equals(dateProvider.getTomorrow())).isFalse();
+        assertThat(this.jobSchedulingService.getSchedulingDataProcessingService()
+                .getAvailableResourcesForGivenFacultyForGivenDay(dateProvider.getTomorrow(), "AE"))
+                .isEqualTo(List.of(new FacultyDatedResourcesResponseModel(dateProvider.getTomorrow(), "AE", 4.0, 2.0, 4.0)));
+
+    }
+}
